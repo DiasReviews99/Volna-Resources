543e8c72e7bf620c6cff9cbe7a32b7e3bbe97e10
DiasReviews99 <73920660+DiasReviews99@users.noreply.github.com>
2021-01-28 11:22:44 +0600

    Create ubchromiumfix.sh

---
+++ Scripts/Fixes/ubchromiumfix.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+clear
+echo "===================================================================="
+echo "Removing Distribution Provided Chromium Packages and Dependencies..."
+echo "===================================================================="
+sleep 1
+apt purge chromium* chromium-browser* snapd -y -qq && apt autoremove -y -qq
+sudo apt purge chromium* chromium-browser* -y -qq && apt autoremove -y -qq
+apt update -qq; apt install software-properties-common gnupg --no-install-recommends -y -qq
+clear
+echo "============================================"
+echo "Adding Debian repo for Chromium installation"
+echo "============================================"
+sleep 1
+
+echo "deb http://ftp.debian.org/debian buster main
+deb http://ftp.debian.org/debian buster-updates main" >> /etc/apt/sources.list
+
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys DCC9EFBF77E11517
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 648ACFD622F3D138
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys AA8E81B4331F7F50
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 112695A0E562B32A
+
+apt update -y
+apt install chromium -y
+
+sed -i 's/chromium %U/chromium --no-sandbox %U/g' /usr/share/applications/chromium.desktop
