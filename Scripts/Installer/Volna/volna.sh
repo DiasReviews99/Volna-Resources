3e269d02e3030d5a1b52c493b6018879911e7117
DiasReviews99 <73920660+DiasReviews99@users.noreply.github.com>
2021-02-03 10:26:57 +0600

    Update volna.sh

--- Scripts/Installer/Volna/volna.sh
+++ Scripts/Installer/Volna/volna.sh
@@ -166,7 +166,21 @@ echo "#!/bin/bash
 rm -rf /etc/resolv.conf
 echo 'nameserver 8.8.8.8' >> /etc/resolv.conf
 mkdir -p ~/.vnc
-./.bash_profile
+clear
+echo "=================="
+echo "Basic Installation"
+echo "=================="
+rm -rf /etc/resolv.conf
+echo "nameserver 8.8.8.8" >> /etc/resolv.conf
+mkdir -p ~/.vnc
+apt update && apt install dialog ubuntu-minimal -y
+groupadd --gid 3003 permagrp
+groupadd --gid 9997 permagrp2
+groupadd --gid 50110 permagrp3
+groupadd --gid 99909997 permagrp4
+groupadd --gid 20195 permagrp5
+groupadd --gid 50195 permagrp6
+touch ~/.hushlogin
 apt update -y && apt install sudo dialog wget -y > /dev/null
 clear
 if [ ! -f /root/xfce19.sh ]; then
