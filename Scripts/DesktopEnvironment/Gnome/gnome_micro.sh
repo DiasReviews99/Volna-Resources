40e2bf4902b5658f14c90a4f9067aba046e597f0
DiasReviews99 <73920660+DiasReviews99@users.noreply.github.com>
2021-02-24 13:53:23 +0600

    Create gnome_micro.sh

---
+++ Scripts/DesktopEnvironment/Gnome/gnome_micro.sh
@@ -0,0 +1,19 @@
+#!/data/data/com.termux/files/usr/bin/bash
+clear
+echo "=============================="
+echo "Installing Desktop Environment"
+echo "=============================="
+sleep 1
+apt install xfce4 -y
+apt install xfce4-terminal -y
+apt install netsurf -y
+clear
+echo "===================="
+echo "Installing Programms"
+echo "===================="
+apt install python2 -y
+apt install perl -y
+cpan App::cpanminus
+apt install build-essential -y
+cpanm Module::build-essential
+apt install mtpaint -y
